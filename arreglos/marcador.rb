dba1818f70b60a0572589fc9d1aeb844c56f1e79
carpenaloza <car.penaloza@hotmail.es>
2021-07-13 23:36:36 -0400

    marcador

--- arreglos/marcador.rb
+++ arreglos/marcador.rb
@@ -1,5 +1,6 @@
 # Datos de entrada
-marcador ="3-4" # Cadena de texto o caracteres
+puts "Ingrese un marcador:"
+marcador = gets # Cadena de texto o caracteres
 
 marcador_local = marcado. split("-")[0].to_i
 marcador_visitante = marcador.split("-")[1].to_i
@@ -9,15 +10,14 @@ marcador_visitante = marcador.split("-")[1].to_i
 #condicionales
 if marcador_local > marcador_visitante
     mensaje1 = "El equipo local es el ganador"
+    puts mensaje1
 elsif marcador_local < marcador_visitante
     mensaje2 = "El equipo visitante es el ganador"
+    puts mensaje2
 elsif marcador_local == marcador_visitante
     mensaje3 = "Empate"
+    puts mensaje3
 
-
-puts marcador_local > marcador_visitante
-puts marcador_local < marcador_visitante
-puts marcador_local == marcador_visitante
 #puts texto_prueba.split(".6.")
 
 #Datos de salida

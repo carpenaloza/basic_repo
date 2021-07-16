3611c726c56f4f153a6fa91fa3bc52484cfcc458
carpenaloza <car.penaloza@hotmail.es>
2021-07-15 23:09:26 -0400

    parto terminado

--- sumarestaparesetc.rb
+++ sumarestaparesetc.rb
@@ -31,23 +31,18 @@ while vueltas >= 0
         for i in 0..arreglo_numeros.length
             suma += arreglo_numeros[i].to_i
         end
-        # puts arreglo_numeros[0].to_i + arreglo_numeros[1].to_i + arreglo_numeros[2].to_i
+
         puts "La suma total es #{suma} de la lista de los números"
-        puts "La suma total es " + suma.to_s + " de la lista de los números"
-        puts 'La suma total es #{suma} de la lista de los números'
+        #puts "La suma total es " + suma.to_s + " de la lista de los números"
+        #puts 'La suma total es #{suma} de la lista de los números'
+        # Cualquiera de las tres formas descritas muestra el resultado.
 
     elsif opcion == "resta"
 
         for i in 0..arreglo_numeros.length
-            suma += arreglo_numeros[i].to_i
-            if i % 2 == 0
-
-                suma += i
-                #numero par
-    
-            end
+            resta -= arreglo_numeros[i].to_i
         end
-        puts "La suma total es #{resta} de la lista de los números"
+        puts "La resta total es #{resta} de la lista de los números"
         
         
     elsif opcion == "pares"
@@ -63,7 +58,7 @@ while vueltas >= 0
                 end
                 
             end
-            puts "La suma total de pares es #{pares} de la lista de los números"
+            #puts "La suma total de pares es #{pares} de la lista de los números"
             puts "La suma total es " + pares.to_s + " de la lista de los números"
         
 
@@ -78,7 +73,7 @@ while vueltas >= 0
         #end
         #puts "Cantidad de numeros pares: #{pares} "
 
-        # si no, finalizar
+        
         
     
 
@@ -87,22 +82,25 @@ while vueltas >= 0
             for n in convertidos_a_numeros
 
                 if n % 2 != 0
-
+                    #!= hace que el residuo si es distinto a 0 es impar el número.
                     impares = impares + 1
-                    #numero par
+                    #numero impar
             
                 end
             end
             puts "La suma total de pares es #{impares} de la lista de los números"
             # contar los números impares
     elsif opcion == "mayor"
+        mayor = [1, 4, 5, 7, 46, 87, 4, 6, 8, 9, 200, 6, 4, 5, 78, 34, 567].sort
+        mayor = mayor.last
         
-        
+        puts "El número mayor de la lista es #{mayor}"
 
     elsif opcion == "menor"
-        # encontrar el número menor
-    
-        # mostrar mensaje de error
+        menor = [1, 4, 5, 7, 46, 87, 4, 6, 8, 9, 200, 6, 4, 5, 78, 34, 567].sort
+        menor = menor.first
+        
+        puts "El número menor de la lista es #{menor}"
     end
 
 end

2b339c264f768559063c0639131de7ca806d6eeb
carpenaloza <car.penaloza@hotmail.es>
2021-07-20 02:04:48 -0400

    vetmain

---
+++ classmascotas/mascota.rb
@@ -0,0 +1,30 @@
+# class Cliente
+#     attr_accessor :nombre_cliente
+#     attr_accessor :fecha
+#     attr_accessor :rut
+#     attr_accessor :domicilio
+
+
+#     # def mostrar_mascotas
+#     #     puts "#{@nombre} #{@mascota}"
+#     # end
+# end
+class Mascota
+    attr_accessor :nombre
+    attr_accessor :size 
+    attr_accessor :edad 
+    attr_accessor :raza
+
+    def mostrar_datos
+        puts "
+        Nombre: #{@nombre} \t 
+        Tamaño: #{@size} \t 
+        Edad: #{@edad} \t 
+        Raza: #{@raza}"
+    end
+end
+
+        # Nombre Cliente: #{@nombre_cliente} \t 
+        # Fecha de registro: #{@fecha} \t 
+        # Rut: #{@rut} \t 
+        # Domicilio: #{@domicilio} \t

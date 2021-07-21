b8b774d154b764530f5d264790eb5273098d9f70
YOUR NAME <car.penaloza@hotmail.es>
2021-07-21 09:56:11 -0400

    arreglando archivos

--- classmascotas/cliente.rb
+++ classmascotas/cliente.rb
@@ -1,12 +1,12 @@
 
 class Cliente
-    @nombre_cliente
     attr_accessor :nombre_cliente
     attr_accessor :fecha
     attr_accessor :rut
     attr_accessor :domicilio
+    attr_accessor :mascota
 
     def mostrar_mascota
-        puts "nombre: #{@nombre} y mi mascota: #{@mascota.nombre}"
+        puts "nombre: #{@nombre_cliente} y mi mascota: #{@mascota.nombre}"
     end
 end
--- classmascotas/main.rb
+++ classmascotas/main.rb
@@ -11,7 +11,7 @@ require_relative 'generador_clientes'
 #Utilizando un método, mostrar la información del cliente
 # lista_de_mascota = []
 
-clientes_sin_mascotas = GeneradorClientes.generar_sin_mascotas(40)
+clientes_sin_mascotas = GeneradorClientes.generar_sin_mascotas(99)
 
 lista_gatos = []
 lista_perros = []
@@ -22,7 +22,7 @@ for i in 0..50
     perro_nuevo.size = Faker::Creature::Dog.coat_length
     perro_nuevo.edad = Faker::Creature::Dog.age
     perro_nuevo.raza = Faker::Creature::Dog.breed
-    perro_nuevo.mostrar_datos
+    #perro_nuevo.mostrar_datos
     #perro_nuevo.ladrar
     lista_perros << perro_nuevo
 end
@@ -33,7 +33,7 @@ for i in 0..50
     gato_nuevo.size = Faker::Creature::Dog.coat_length
     gato_nuevo.edad = Faker::Creature::Dog.age
     gato_nuevo.raza = Faker::Creature::Cat.breed
-    gato_nuevo.mostrar_datos
+    #gato_nuevo.mostrar_datos
     #gato_nuevo.maullar
 
     lista_gatos << gato_nuevo
@@ -51,14 +51,14 @@ end
 # end
 
 for i in 0..49
-    lista_de_cliente[i].mascota = lista_gatos[i]
+    clientes_sin_mascotas[i].mascota = lista_gatos[i]
 end
 
 for i in 50..99
-    lista_de_cliente[i].mascota = lista_perros[i - 50]
+    clientes_sin_mascotas[i].mascota = lista_perros[i - 50]
 end
 
-for cliente in lista_de_cliente
+for cliente in clientes_sin_mascotas
     cliente.mostrar_mascota
 end
 #File.write("Clientes.txt", lista_de_clientes)
@@ -72,10 +72,10 @@ end
 nuevos_clientes = []
 for i in 0..100
     cliente = Cliente.new
-    cliente.nombre = Faker::Name.name
+    cliente.nombre_cliente = Faker::Name.name
     #asignación de mascota
     cliente.mascota = Mascota.new
-    cliente.mascota.nombre = Faker::Creature::Name.name
+    cliente.mascota.nombre = Faker::Creature::Animal.name
 
 
     cliente.mostrar_mascota

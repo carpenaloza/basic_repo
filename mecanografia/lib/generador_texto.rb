1201ba417202022b2ee1000e261dafb8093d909d
carpenaloza <car.penaloza@hotmail.es>
2021-07-26 10:47:04 -0400

    tdd mecanografia

---
+++ mecanografia/lib/generador_texto.rb
@@ -0,0 +1,3 @@
+class GeneradorTexto
+    
+end
---
+++ mecanografia/spec/generador_texto_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../lib/generador_texto"
+
+RSpec.describe GeneradorTexto do
+
+    it "Con solo letras" do
+        
+    end
+
+    it "Con palabras cortas" do
+        
+    end
+
+    it "Con combinaciones de teclas" do
+        
+    end
+
+end

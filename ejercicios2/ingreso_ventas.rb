ad523db108e93ca3686eb304e350327e2836086e
carpenaloza <car.penaloza@hotmail.es>
2021-08-05 21:28:52 -0400

    ejercicio2 ingreso ventas

--- ejercicios2/ingreso_ventas.rb
+++ ejercicios2/ingreso_ventas.rb
@@ -2,15 +2,12 @@
 
 class MenuSubmenuConsola
     
-
-
-    
     
         def main()
             puts "Ingrese el valor del número total de ventas realizados el día de hoy"
             n = gets.chomp.to_i
 
-            print "Menu\n1. Total de ventas [1]\n2. Promedio de ventas [2]\nFavor ingresar opcion [1_2]: ";
+            print "Menu\n1. Ingreso de ventas [1]\nFavor ingresar opcion [1]: ";
             opcion = (gets.chomp).to_i
             
     
@@ -35,23 +32,32 @@ class MenuSubmenuConsola
                         end
                     end
                     for n in numeros
-                        print n.to_s + " / "
+                        print n.to_s + " - "
                     end
-                    print " = " + suma.to_s
+
+                    promedio = suma/total.length
+
+                    # print " = " + suma.to_s
                     puts
+
+                    print "SubMenu\n1. Total de ventas [1]\n2. Promedio de ventas [2]\nFavor ingresar opcion [1_2]: ";
+                    opcion = (gets.chomp).to_i
 
                     #InvocarFuncion1_1()
                     # print "\nVentas Totales\n"
                     
                     # puts "El total de ventas del día es #{suma}"
                     
-                    
-                when 2
-                    # print "\nPromedio de ventas\n"
-                    
-                    # puts "El promedio de tus ventas del día fue de #{promedio}"
-                else
-                    # print "\nError de opción de menú\n"
+                    case opcion
+                        when 1                            
+                            puts "Sus ventas fueron #{total}"
+                            puts "El total de tus ventas del día fue de #{suma}"
+                        when 2                            
+                            puts "Sus ventas fueron #{total}"
+                            puts "El promedio de tus ventas del día fue de #{promedio}"
+                        else
+                            print "\nError de opción de menú\n"
+                        end
             end
         
         
